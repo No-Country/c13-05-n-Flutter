11a0f1535708af8857210d92e99495935da5299f
Gabriel Iciarte <gabriel_viki@hotmail.com>
2023-08-17 21:03:18 -0300

    first commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color.fromRGBO(27, 156, 221, 1),
+      );
+}
