ed620d208557d823fcc52e8d1ca7f270af87d92d
Gabriel Iciarte <gabriel_viki@hotmail.com>
2023-08-20 16:00:28 -0300

    login, boton oculto

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class AppTheme {
   ThemeData getTheme() => ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: const Color.fromRGBO(27, 156, 221, 1),
+        colorSchemeSeed: const Color.fromARGB(255, 25, 139, 196),
       );
 }
